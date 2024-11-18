78a4cc6c2b7e6685ae1d3680a489f4186c73caf0
ShakerFayez <shakirfayez21@gmail.com>
2024-11-19 00:25:56 +0200

    Added the Linear Regression algorithm and its tests

--- utils.py
+++ utils.py
@@ -3,3 +3,7 @@ import numpy as np
 # computes the euclidean distance
 def euclidean_distance(x1, x2):
     return np.sqrt(np.sum(x2-x1)**2)
+
+# computes the mean squared error
+def mse(y_true, y_predicted):
+    return np.mean((y_true - y_predicted)**2)
