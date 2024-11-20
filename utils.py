c768cc4b4e09e9eb6519e7d90c11675bae291ec9
ShakerFayez <shakirfayez21@gmail.com>
2024-11-20 15:17:50 +0200

    Added Logistic Regression and its tests

--- utils.py
+++ utils.py
@@ -7,3 +7,7 @@ def euclidean_distance(x1, x2):
 # computes the mean squared error
 def mse(y_true, y_predicted):
     return np.mean((y_true - y_predicted)**2)
+
+# computes the accuracy score
+def accuracy_score(y_true, y_predicted):
+    return np.sum(y_true == y_predicted) / len(y_true)
